c7e19c99896e2c0e4987415548ec6963963cbd50
gamxu <wuttipat.did@gmail.com>
2024-01-24 20:35:39 +0700

    backend first init

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # CPEvent-Backend
+
+**library included:**
+
+- Air use`air` to run
+- Godotenv
+- Gin
+- Gorm
---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"log"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+	  log.Fatal("Error loading .env file")
+	}
+
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run() // listen and serve on 0.0.0.0:8080
+}
