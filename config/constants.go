f6019a126645ddb44cce0d602f60b17e627ac47a
Nuttadon <nuttadon_p@cmu.ac.th>
2024-01-30 16:18:12 +0700

    Google Authen information extract

---
+++ config/constants.go
@@ -0,0 +1,22 @@
+package config
+
+import (
+	"os"
+
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
+)
+
+func SetupConfig() *oauth2.Config {
+	conf := &oauth2.Config{
+		RedirectURL:  "http://localhost:4000/auth/callback", // Set this to your callback URL
+		ClientID:     os.Getenv("GOOGLE_ID"),
+		ClientSecret: os.Getenv("GOOGLE_SECRET"),
+		Scopes: []string{
+			"https://www.googleapis.com/auth/userinfo.email",
+			"https://www.googleapis.com/auth/userinfo.profile",
+			"https://www.googleapis.com/auth/user.gender.read,"},
+		Endpoint: google.Endpoint,
+	}
+	return conf
+}
