6bc367a3e8cfb3e44d5fcfd3944702fae08a3db6
Ixac <90751114+Ixac@users.noreply.github.com>
2024-02-25 14:28:20 +0700

    add Fn DeleteEXP

--- controllers/expController.go
+++ controllers/expController.go
@@ -146,4 +146,40 @@ func EditExperience(c *gin.Context) {
     c.JSON(http.StatusOK, gin.H{"experience": experience})
 }
 
+func DeleteExperience(c *gin.Context) {
+    // Get profile ID and experience ID from the request parameters
+    profileID, err := strconv.ParseUint(c.Param("profileID"), 10, 64)
+    if err != nil {
+        c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid ProfileID"})
+        return
+    }
+
+    // Find the profile by ID
+    var profile models.Profile
+    if err := initializers.DB.First(&profile, profileID).Error; err != nil {
+        c.JSON(http.StatusNotFound, gin.H{"error": "Profile not found"})
+        return
+    }
+
+    expID, err := strconv.ParseUint(c.Param("expID"), 10, 64)
+    if err != nil {
+        c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid ExpID"})
+        return
+    }
+
+    // Check if the experience exists and belongs to the correct profile
+    var experience models.Exp
+    if err := initializers.DB.Where("profile_id = ? AND id = ?", profileID, expID).First(&experience).Error; err != nil {
+        c.JSON(http.StatusNotFound, gin.H{"error": "Experience not found or does not belong to the profile"})
+        return
+    }
+
+    // Delete the experience
+    if err := initializers.DB.Delete(&experience).Error; err != nil {
+        c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to delete experience"})
+        return
+    }
+
+    c.JSON(http.StatusOK, gin.H{"message": "Experience deleted successfully"})
+}
 
